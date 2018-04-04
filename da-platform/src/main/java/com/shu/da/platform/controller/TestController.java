b53887f69f9a7c8d2dd4bb4d69edcad3e034508b
shudongping <526826717@qq.com>
2018-04-04 18:03:10 +0800

    init project

--- da-platform/src/main/java/com/shu/da/platform/controller/TestController.java
+++ da-platform/src/main/java/com/shu/da/platform/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.shu.da.platform.controller;
 
+import com.shu.da.platform.kettle.JobManager;
 import com.shu.da.platform.kettle.KettleUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.pentaho.di.core.logging.KettleLogStore;
+import org.pentaho.di.core.logging.LoggingBuffer;
 import org.pentaho.di.job.Job;
 import org.pentaho.di.job.JobMeta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @Slf4j
 public class TestController {
+
+
+
 
     /**
      * 从数据库资源库获取job
@@ -34,9 +41,44 @@ public class TestController {
 
         Job job = new Job(KettleUtils.getInstanceRep(), jobMeta);
 
-        job.start();
+//        Thread.sleep(1000*5);
 
-        return job.getObjectId();
+//        job.start();
+//        job.run();
+//        Thread.sleep(1000*5);
+//        String all_msg = KettleLogStore.getAppender().getBuffer().toString();
+//        String logChannelId = job.getLogChannelId();
+//        LoggingBuffer appender = KettleLogStore.getAppender();
+//        String logText = appender.getBuffer(logChannelId, true).toString();
+
+        String status = JobManager.startJob(job);
+        return status;
     }
+
+    /**
+     * 实时日志
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/job/{jobId}/log")
+    public Object getLog(@PathVariable("jobId") Long jobId) throws Exception{
+        JobMeta jobMeta = KettleUtils.loadJob(jobId);
+
+        Job job = new Job(KettleUtils.getInstanceRep(), jobMeta);
+        String all_msg = KettleLogStore.getAppender().getBuffer().toString();
+        log.info(all_msg);
+        String logChannelId = job.getLogChannelId();
+        LoggingBuffer appender = KettleLogStore.getAppender();
+        String logText = appender.getBuffer(logChannelId, true).toString();
+        log.info(logChannelId+":"+logText);
+        return all_msg;
+    }
+
+    @GetMapping("/log")
+    public Object getTest() throws Exception{
+        return "log success";
+    }
+
+
 
 }
